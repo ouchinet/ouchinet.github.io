f036d8bea92105e36e68d5098699c2b68dca2296
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-12-29 20:08:15 +0900

    一旦リセット

--- js/header.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load",() => {
-    document.querySelector("header").innerHTML =
-        document.querySelector("header").innerHTML +
-        "<a href='/' style='margin:10px'>ホーム</a><a href='/docs' style='margin:10px'>ドキュメント</a>"
-})
