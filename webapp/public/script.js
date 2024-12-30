24f9e996896f27c9a767ec83f1b02d8c5cea16f5
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-12-30 13:45:28 +0900

    無理にでも静的ページに戻した

--- webapp/public/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load",() => {
-    try{
-        document.getElementById("home-latest-release").addEventListener("click", () =>{
-            location.href="https://github.com/ouchinet/ouchinet/releases"
-        })
-    }catch(e){}
-})
